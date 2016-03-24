fcc74819af6ffa246abb041e057e2c1c03c031ed
Konrad K Sobon <ksobon1986@gmail.com>
2016-03-24 16:58:21 -0400

    Update and rename xl_FillStyle.py to Fill Style.py

--- styles/Fill Style.py
+++ styles/Fill Style.py
@@ -1,4 +1,4 @@
-#Copyright(c) 2015, David Mans, Konrad Sobon
+# Copyright(c) 2016, David Mans, Konrad Sobon
 # @arch_laboratory, http://archi-lab.net, http://neoarchaic.net
 
 import clr
@@ -8,13 +8,25 @@ pyt_path = r'C:\Program Files (x86)\IronPython 2.7\Lib'
 sys.path.append(pyt_path)
 
 import os
-import os.path
 appDataPath = os.getenv('APPDATA')
-bbPath = appDataPath + r"\Dynamo\0.8\packages\Bumblebee\extra"
+dynPath = appDataPath + r"\Dynamo\0.9"
+if dynPath not in sys.path:
+	sys.path.Add(dynPath)
+	
+bbPath = appDataPath + r"\Dynamo\0.9\packages\Bumblebee\extra"
 if bbPath not in sys.path:
-	sys.path.Add(bbPath)
-
-import bumblebee as bb
+	try:
+		sys.path.Add(bbPath)
+		import bumblebee as bb
+	except:
+		import xml.etree.ElementTree as et
+		root = et.parse(dynPath + "\DynamoSettings.xml").getroot()
+		for child in root:
+			if child.tag == "CustomPackageFolders":
+				for path in child:
+					if path not in sys.path:
+						sys.path.Add(path)
+		import bumblebee as bb
 
 #The inputs to this node will be stored as a list in the IN variable.
 dataEnteringNode = IN
@@ -22,8 +34,6 @@ dataEnteringNode = IN
 patternType = IN[0]
 backgroundColor = IN[1]
 patternColor = IN[2]
-opacity = IN[3]
-bevelType = IN[4]
 
 fillStyle = bb.BBFillStyle()
 
@@ -33,10 +43,6 @@ if backgroundColor != None:
 	fillStyle.backgroundColor = backgroundColor
 if patternColor != None:
 	fillStyle.patternColor = patternColor
-if opacity != None:
-	fillStyle.opacity = opacity
-if bevelType != None:
-	fillStyle.bevelType = bevelType
 
 #Assign your output to the OUT variable
 OUT = fillStyle
