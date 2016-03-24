047bc0627bc4ad779b71ec927cd675809a23cc96
Konrad K Sobon <ksobon1986@gmail.com>
2016-03-24 16:08:22 -0400

    Update xl_ClearContents.py

--- app/xl_ClearContents.py
+++ app/xl_ClearContents.py
@@ -1,18 +1,10 @@
-#Copyright(c) 2015, David Mans, Konrad Sobon
+# Copyright(c) 2016, David Mans, Konrad Sobon
 # @arch_laboratory, http://archi-lab.net, http://neoarchaic.net
 
 import clr
 import sys
-
-pyt_path = r'C:\Program Files (x86)\IronPython 2.7\Lib'
-sys.path.append(pyt_path)
-
-import os
-import os.path
-appDataPath = os.getenv('APPDATA')
-bbPath = appDataPath + r"\Dynamo\0.8\packages\Bumblebee\extra"
-if bbPath not in sys.path:
-	sys.path.Add(bbPath)
+clr.AddReference('ProtoGeometry')
+from Autodesk.DesignScript.Geometry import *
 
 import System
 from System import Array
@@ -20,11 +12,32 @@ from System.Collections.Generic import *
 
 clr.AddReferenceByName('Microsoft.Office.Interop.Excel, Version=11.0.0.0, Culture=neutral, PublicKeyToken=71e9bce111e9429c')
 from Microsoft.Office.Interop import Excel
-
 System.Threading.Thread.CurrentThread.CurrentCulture = System.Globalization.CultureInfo("en-US")
 from System.Runtime.InteropServices import Marshal
 
-import bumblebee as bb
+pyt_path = r'C:\Program Files (x86)\IronPython 2.7\Lib'
+sys.path.append(pyt_path)
+
+import os
+appDataPath = os.getenv('APPDATA')
+dynPath = appDataPath + r"\Dynamo\0.9"
+if dynPath not in sys.path:
+	sys.path.Add(dynPath)
+	
+bbPath = appDataPath + r"\Dynamo\0.9\packages\Bumblebee\extra"
+if bbPath not in sys.path:
+	try:
+		sys.path.Add(bbPath)
+		import bumblebee as bb
+	except:
+		import xml.etree.ElementTree as et
+		root = et.parse(dynPath + "\DynamoSettings.xml").getroot()
+		for child in root:
+			if child.tag == "CustomPackageFolders":
+				for path in child:
+					if path not in sys.path:
+						sys.path.Add(path)
+		import bumblebee as bb
 
 #The inputs to this node will be stored as a list in the IN variable.
 dataEnteringNode = IN
