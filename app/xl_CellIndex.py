67386e5c8ba22eec488683b5e8c89c024fb4c5aa
Konrad K Sobon <ksobon1986@gmail.com>
2016-03-24 16:06:55 -0400

    Update xl_CellIndex.py

--- app/xl_CellIndex.py
+++ app/xl_CellIndex.py
@@ -1,19 +1,31 @@
-#Copyright(c) 2015, David Mans, Konrad Sobon
+# Copyright(c) 2016, David Mans, Konrad Sobon
 # @arch_laboratory, http://archi-lab.net, http://neoarchaic.net
 
 import clr
 import sys
-
 pyt_path = r'C:\Program Files (x86)\IronPython 2.7\Lib'
 sys.path.append(pyt_path)
 
 import os
 appDataPath = os.getenv('APPDATA')
-bbPath = appDataPath + r"\Dynamo\0.8\packages\Bumblebee\extra"
+dynPath = appDataPath + r"\Dynamo\0.9"
+if dynPath not in sys.path:
+	sys.path.Add(dynPath)
+	
+bbPath = appDataPath + r"\Dynamo\0.9\packages\Bumblebee\extra"
 if bbPath not in sys.path:
-	sys.path.Add(bbPath)
-
-import bumblebee as bb
+	try:
+		sys.path.Add(bbPath)
+		import bumblebee as bb
+	except:
+		import xml.etree.ElementTree as et
+		root = et.parse(dynPath + "\DynamoSettings.xml").getroot()
+		for child in root:
+			if child.tag == "CustomPackageFolders":
+				for path in child:
+					if path not in sys.path:
+						sys.path.Add(path)
+		import bumblebee as bb
 import string
 import re
 
@@ -25,8 +37,6 @@ cellAddress = IN[0]
 def ProcessList(_func, _list):
     return map( lambda x: ProcessList(_func, x) if type(x)==list else _func(x), _list )
 
-# if input is a list iterate over it and return
-# a list of row index and column index for each
 if isinstance(cellAddress, list):
 	OUT = ProcessList(bb.CellIndex, cellAddress)
 else:
